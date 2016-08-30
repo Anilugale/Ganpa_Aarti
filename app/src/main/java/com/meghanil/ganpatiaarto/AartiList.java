8e36bdab93a7cac484e73059ad94b5b0195c7695
anil <anilugale1989@gmail.com>
2016-08-30 15:25:06 +0530

    permission code

--- app/src/main/java/com/meghanil/ganpatiaarto/AartiList.java
+++ app/src/main/java/com/meghanil/ganpatiaarto/AartiList.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -22,7 +23,7 @@ import com.meghanil.ganpatiaarto.adapter.AartiListAdapter;
 public class AartiList extends AppCompatActivity {
     FloatingActionButton fab;
     RecyclerView aartiList;
-    public static int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1001;
+    public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,19 +39,51 @@ public class AartiList extends AppCompatActivity {
         aartiList.setLayoutManager(new LinearLayoutManager(this));
         AartiListAdapter adapter =new AartiListAdapter(this);
         aartiList.setAdapter(adapter);
-        checkPremistion();
+        if(!checkPermission()){
+            requestPermission();
+        }
     }
 
-    private void checkPremistion() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{ Manifest.permission.INTERNET},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        if (result == PackageManager.PERMISSION_GRANTED){
+
+            return true;
+
+        } else {
+
+            return false;
+
         }
+    }
 
+    private void requestPermission(){
 
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.INTERNET)){
+
+            Toast.makeText(this,"internet permission of new updated data of Aarti.",Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this,"internet permission of new updated data of Aarti.",Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(this,"fail.",Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
 
 }
