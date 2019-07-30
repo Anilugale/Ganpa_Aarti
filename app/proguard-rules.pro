ab6af83d92435083c4ecf30b14ead130cd27a274
anilugale1989@gmail.com <anilugale1989@gmail.com>
2019-07-30 11:58:39 +0530

    facebook ad intiat banner integration

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -101,5 +101,4 @@
 	public *;
 }
 
-
 -keep class com.stripe.android.** { *; }
--- app/src/main/java/com/meghanil/ganpatiaarto/AartiList.kt
+++ app/src/main/java/com/meghanil/ganpatiaarto/AartiList.kt
@@ -1,22 +1,28 @@
 package com.meghanil.ganpatiaarto
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
-
+import com.facebook.ads.Ad
+import com.facebook.ads.AdError
+import com.facebook.ads.InterstitialAd
+import com.facebook.ads.InterstitialAdListener
 import com.meghanil.ganpatiaarto.adapter.AartiListAdapter
 import kotlinx.android.synthetic.main.aarti_list.*
 
+
 class AartiList : AppCompatActivity() {
+    lateinit var interstitialAd: InterstitialAd
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.aarti_list)
         init()
         AppRater.app_launched(this)
+
         Log.d(TAG,"onCreate END")
     }
 
@@ -24,8 +30,70 @@ class AartiList : AppCompatActivity() {
         aartiList.layoutManager = LinearLayoutManager(this)
         val adapter = AartiListAdapter(this)
         aartiList.adapter = adapter
+        interstitialAd = InterstitialAd(this, "2416433748619591_2417702178492748")
+
+
+        interstitialAd.setAdListener(object : InterstitialAdListener {
+            override fun onInterstitialDisplayed(ad: Ad?) {
+                // Interstitial ad displayed callback
+
+                Log.e(TAG, "Interstitial ad displayed.")
+            }
+
+            override fun onInterstitialDismissed(ad: Ad?) {
+                // Interstitial dismissed callback
+
+                finish()
+                Log.e(TAG, "Interstitial ad dismissed.")
+            }
+
+            override fun onError(ad: Ad?, adError: AdError) {
+                // Ad error callback
+
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.errorMessage)
+            }
+
+            override fun onAdLoaded(ad: Ad?) {
+                // Interstitial ad is loaded and ready to be displayed
+
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!")
+                // Show the ad
+
+
+
+            }
+
+            override fun onAdClicked(ad: Ad?) {
+                // Ad clicked callback
+
+                Log.d(TAG, "Interstitial ad clicked!")
+            }
+
+            override fun onLoggingImpression(ad: Ad?) {
+                // Ad impression logged callback
+
+                Log.d(TAG, "Interstitial ad impression logged!")
+            }
+        })
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+
+
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        // For auto play video ads, it's recommended to load the ad
+        // at least 30 seconds before it is shown
+        interstitialAd.loadAd()
     }
 
+    override fun onBackPressed() {
+        if(interstitialAd.isAdLoaded){
+            interstitialAd.show()
+        }else {
+            super.onBackPressed()
+        }
+    }
     companion object {
         const val TAG = "AartiList"
     }
